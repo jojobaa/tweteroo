6e185cb29c42367b20628ef8e5b039a1b569b7c3
joy <joycarvalho59@gmail.com>
2022-11-07 14:21:51 -0300

    version final

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post("/sign-up", (req, res) => {
     if (!req.body.username || !req.body.avatar) {
         res.status(400).send("Todos os campos são obrigatórios");
         // return
-      }
+    }
 
     user.push(req.body);
     res.status(201).send("OK");
@@ -23,23 +23,38 @@ app.post("/tweets", (req, res) => {
     if (!req.body.tweet) {
         res.status(400).send("Todos os campos são obrigatórios");
         // return
-      }
+    }
     const userAvatar = user.find(info => info.username === req.headers.user)?.avatar
     tweets.push({
-		username: req.headers.user,
-		avatar: userAvatar,
-		tweet: req.body.tweet
-	});
+        username: req.headers.user,
+        avatar: userAvatar,
+        tweet: req.body.tweet
+    });
     res.status(201).send("OK");
 })
 
 app.get("/tweets", (req, res) => {
-  if(tweets.length <= 10){
-    res.send([...tweets].reverse());
-  } else {
-    const lastTweets = tweets.slice(tweets.length - 10, tweets.length).reverse();
-    res.send(lastTweets);
-  }
+    const page = parseInt(req.query.page)
+    if (!page && page < 1) {
+        res.status(400).send("informe uma pagina valida")
+        return
+    }
+    if (page === 1) {
+        res.send(tweets.slice(- 10).reverse());
+        return
+    }
+    res.send(tweets.slice(-10 * page, -10 * page + 10).reverse());
+})
+
+app.get("/tweets/:username", (req, res) => {
+    const username = req.params.username;
+    const userPosts = tweets.filter(infoPost => infoPost.username === username)
+    if (userPosts.length <= 10) {
+        res.send([...userPosts].reverse());
+    } else {
+        const lastUserPosts = userPosts.slice(userPosts.length - 10, userPosts.length).reverse();
+        res.send(lastUserPosts);
+    }
 })
 
 app.listen(5000);
